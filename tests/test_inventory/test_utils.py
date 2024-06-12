d7073e53218727f69d0933559bc3797d068ffb65
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-06-12 08:53:52 +0100

    Create test_utils.py

---
+++ tests/test_inventory/test_utils.py
@@ -0,0 +1,14 @@
+import pytest
+from rest_framework.response import Response
+from apps.inventory.utils import format_response
+
+def test_format_response_with_data():
+    response = format_response(200, "Success", {"key": "value"})
+    assert isinstance(response, Response)
+    assert response.data == {"responseCode": 200, "message": "Success", "data": {"key": "value"}}
+
+def test_format_response_without_data():
+    response = format_response(200, "Success")
+    assert isinstance(response, Response)
+    assert response.data == {"responseCode": 200, "message": "Success", "data": {}}
+  
