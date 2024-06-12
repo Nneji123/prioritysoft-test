8599cc34355db7c0f372913a203e68f5f6556ec6
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-06-12 10:48:19 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,14 @@
 import sys
 import os
 
+from __future__ import absolute_import
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from ..store.celery import app as celery_app
+
+__all__ = ("celery_app",)
+
 # Add the apps directory to the Python path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "apps"))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "staticfiles"))
