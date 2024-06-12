d53a4005b9653862b6adb567c03b246599f35d17
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2024-06-12 08:19:01 +0100

    Update base.py

--- store/settings/base.py
+++ store/settings/base.py
@@ -48,10 +48,8 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
     "core.middlewares.DisableCSRFMiddleware",  # change in production
-    "request_logging.middleware.LoggingMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
+       "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
